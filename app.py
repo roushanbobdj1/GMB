c1fc9c58d2e4701e4ab2661d632823fb3dc86708
roushanbobdj1 <roushan7474@gmail.com>
2026-08-17 14:57:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,8 +54,8 @@ app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
 }
 
 # Real Email Settings (Active)
-app.config['MAIL_SERVER'] = 'smtp-relay.brevo.com'
-app.config['MAIL_PORT'] = 2525                     
+app.config['MAIL_SERVER'] = 'smtp.gmail.com''
+app.config['MAIL_PORT'] = 587                     
 app.config['MAIL_USE_TLS'] = True                 
 app.config['MAIL_USE_SSL'] = False                  
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
