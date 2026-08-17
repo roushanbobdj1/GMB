078d6c69e3032c6ef8fcbc4a29641d8d7c7f5152
roushanbobdj1 <roushan7474@gmail.com>
2026-08-17 14:32:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,9 +55,9 @@ app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
 
 # Real Email Settings (Active)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 2525                     
-app.config['MAIL_USE_TLS'] = False                 
-app.config['MAIL_USE_SSL'] = True                  
+app.config['MAIL_PORT'] = 587                     
+app.config['MAIL_USE_TLS'] = True                 
+app.config['MAIL_USE_SSL'] = False                  
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_APP_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
